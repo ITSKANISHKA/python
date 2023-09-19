3eb8b0bd996c54ec01082cb6e72b1901caa1d0b2
ITSKANISHKA <142567330+ITSKANISHKA@users.noreply.github.com>
2023-09-19 23:26:15 +0530

    Add files via upload

---
+++ random.py
@@ -0,0 +1,26 @@
+'''random module'''
+import random
+y=random.randrange(1,101)
+while True:
+    n=int(input("enter a number"))
+    if n==y:
+        print("you win")
+        break
+    elif n<y:
+        print("too small")
+    else:
+        print("too big")
+        
+    
+
+    
+        
+    
+
+
+
+
+
+
+
+
